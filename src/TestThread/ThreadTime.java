6a697c8a613ea40e1c9f879204771e8477d6eb11
xAm64 <48089283+xAm64@users.noreply.github.com>
2024-01-26 14:40:34 +0100

    Correction horloge

--- src/TestThread/ThreadTime.java
+++ src/TestThread/ThreadTime.java
@@ -1,68 +1,68 @@
 package TestThread;
 
 import javax.swing.*;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 import java.awt.*;
 import java.awt.event.*;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ThreadTime {
-
 	public static void main(String[] args) {
-	    viewClock();
+	    new ViewClock();
 	  }
-	
-	//interface graphique
-	public static void viewClock() {
+}
+
+//interface graphique
+class ViewClock {
+	public ViewClock() {
 		EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
 				try {
 					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); //Appelle l'interface graphique
-				} catch (ClassNotFoundException er) { //Si class pas trouvé
-				} catch (InstantiationException er) { //Si l'instanciation échoue
-				} catch (IllegalAccessException er) { //Si un accès non autorié est détecté
-				} catch (UnsupportedLookAndFeelException e) { //Si une action non supporté est détecté
+				} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException er) {
 				}
 				JFrame lookClock = new JFrame("Horloge"); //Titre de la fenêtre
+				lookClock.getContentPane().setBackground(Color.decode("#96B1C9")); //change la couleur de fond.
 				lookClock.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //si je ferme ça quitte le pgogramme
 				lookClock.setLayout(new BorderLayout()); //Créer la bordure de la fenêtre
 				lookClock.add(new Clock()); //Appelle l'horloge
 				lookClock.pack();
 				lookClock.setLocationRelativeTo(null); //Définie un emplacement spécifique
-				lookClock.setVisible(false); //rends la fenPetre visible
+				lookClock.setVisible(true); //rends la fenPetre visible
 			}
 		});
 	}
-	
-	//méthode pour afficher l'horloge
-	public static class Clock extends JPanel{
-		private JLabel horloge;
-		public Clock() {
-			setLayout(new BorderLayout());
-			horloge = new JLabel();
-			horloge.setHorizontalAlignment(JLabel.CENTER);
-			horloge.setFont(
-					UIManager.getFont("Label.font").deriveFont(Font.BOLD, 50f)
+}
+//méthode pour afficher l'horloge
+class Clock extends JPanel{
+	private JLabel horloge;
+	public Clock() {
+		setLayout(new BorderLayout());
+		horloge = new JLabel(); //créer une nouvelle horloge
+		horloge.setForeground(Color.decode("#232107")); //change la couleur de police
+		horloge.setHorizontalAlignment(JLabel.CENTER); //Alligne l'horloge au centre de la fenêtre
+		horloge.setFont( //Change la police de l'horloge
+				UIManager.getFont("Label.font").deriveFont(Font.BOLD, 50f)
 				);
-			horloge.setText(
-					new SimpleDateFormat("H:mm:ss").format(new Date())
+		horloge.setText( //Applicque le format Heure(1 chiffre si en dessous de 10), minutes, secondes.
+				new SimpleDateFormat("H:mm:ss").format(new Date())
 				);
-			add(horloge);
-			Timer z = new Timer(1000, new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent x) {
-					horloge.setText(
-							new SimpleDateFormat("H:mm:ss").format(new Date())
+		add(horloge); //ajoute l'horloge au point graphique.
+		Timer z = new Timer(1000, new ActionListener() { //rafréchis la fenêtre toutes les secondes.
+			@Override
+			public void actionPerformed(ActionEvent x) { //Change le contenue de l'anciene horloge par la nouvelle.
+				horloge.setText(
+						new SimpleDateFormat("H:mm:ss").format(new Date())
 						);
-					
-				}
-			});
-			z.setRepeats(true);
-			z.setCoalesce(true);
-			z.setInitialDelay(0);
-			z.start();
-		}
+			}
+		});
+		z.setRepeats(true); //Permet de répeter l'action
+		z.setCoalesce(true);  //convertit l'heure en string
+		z.setInitialDelay(0); //décalage du temps initiale
+		z.start(); //démarre l'horloge
 	}
 }
